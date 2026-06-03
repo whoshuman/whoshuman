738fadb7551bbdbcbd44b52d04cdc183f6452f7b
Edu lu <edulu@MacBook-Pro-de-Edu.local>
2026-06-03 17:29:36 +0200

    fix/ActionCache añadir pre-push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
